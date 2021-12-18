a6d63ce35a8633d0389b4472b7aceb417fe23e69
OKirsenko <senka3434@gmail.com>
2021-12-18 17:25:17 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+console.log(getRandomHexColor());
+
+const changeBtn = document.querySelector('.change-color');
+const colorSpan = document.querySelector('.color');
+const body = document.querySelector('body');
+
+
+const onBtnClick = () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+}
+
+changeBtn.addEventListener('click', onBtnClick)
