2402b1a59b99a58990622f2b3f6478b87cb32041
OKirsenko <senka3434@gmail.com>
2021-12-18 13:06:59 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,1 +1,18 @@
-counterValue = 0;
+let counterValue = 0;
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const valueCount = document.querySelector('#value');
+
+const onClickDecrementBtn = () => {
+    counterValue -= 1;
+    valueCount.textContent = counterValue;
+};
+const onClickIncrementBtn = () => {
+    counterValue += 1;
+    valueCount.textContent = counterValue;
+};
+
+decrementBtn.addEventListener('click', onClickDecrementBtn);
+incrementBtn.addEventListener('click', onClickIncrementBtn);
+
