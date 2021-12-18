025f7a15b7b233dd3ad1b1702307c68d7e6d2af2
OKirsenko <senka3434@gmail.com>
2021-12-18 17:08:23 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -2,13 +2,20 @@ const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", handleSubmit);
 
+const user = {};
+
 function handleSubmit(event) {
-  event.preventDefault();
+    event.preventDefault();
+    
   const {
     elements: { email, password }
   } = event.currentTarget;
 
     if (email.value === "" || password.value === "") {
     return alert("Please fill in all the fields!");
-  }
+    }
+    user.email = email.value;
+    user.password = password.value;
+    console.log(user);
+    form.reset();
 }
