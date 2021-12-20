e91f0e920a48d2de625ab29dbba9bef1051c2b59
OKirsenko <senka3434@gmail.com>
2021-12-20 17:12:25 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,33 @@ function getRandomHexColor() {
 }
 
 const controls = document.querySelector('#controls');
+const number = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+
+
+const createBoxes = (amount) => {
+  amount = number.value;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.append(box);
+    
+  }
+}
+
+const destroyBoxes = () => {
+  const divs = boxes.querySelectorAll('div');
+  
+  divs.forEach(div => {
+    div.remove();
+  });
+}
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
